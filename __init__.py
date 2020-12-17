ca711cc9fd842e2749031c26f96e310634169e7c
maragraziani <1537639@gmail.com>
2020-12-17 16:02:28 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,1 @@
-from .rcv import rcv
+from rcv-tool.rcv import get_color_measure, get_all_color_measures, get_texture_measure, get_all_texture_measures, get_batch_activations, linear_regression, compute_mse, compute_rsquared, get_rcv
